ad7c3d7b7ca256936b05cdef36c9b9f6a034ecab
Amar2050 <amarboutayeb@gmail.com>
2021-01-05 16:42:34 +0100

    Boucle for classique 1 à 10
    Rien dans le controller
    Pas besoin de tableau

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,17 +13,9 @@ class HomeController extends AbstractController
      * @Route("/home", name="home")
      */
     public function index(): Response
-    {
-        // Je crée un tableau associatif
-        $games = [
-            "StarCraft 2" => 8,
-            "BF6" => 128,
-            "Métro Exodus" => 1
-        ];
-        
+    {   
         return $this->render('home/index.html.twig', [
-            'name'  => 'Page d\'accueil',
-            "games" => $games
+            'name'  => 'Page d\'accueil'
         ]);
     }
 }
