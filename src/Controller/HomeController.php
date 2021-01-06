e4bc96dfcd87d46ed4beebe68ade20d86797f8ab
Amar2050 <amarboutayeb@gmail.com>
2021-01-06 10:32:56 +0100

    Interet du ficher BASE - Template sans BASE
    Deux vues qui se repetent
    On n'utilise pas le fichier base ni l'héritage de template
    Titre d'exemple....Si 40 pages a faire...Très laborieux !

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,20 +2,30 @@
 
 namespace App\Controller;
 
-use stdClass;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index()
     {    
         return $this->render('home/index.html.twig', [
-            'name'  => 'Page d\'accueil'
+           
         ]);
     }
+
+    /**
+     * @Route("/articles", name="articles_list")
+     */
+    public function articlesList()
+    {    
+        return $this->render('home/articles.list.html.twig', [
+           
+        ]);
+    }
+
+
 }
