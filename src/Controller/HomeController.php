bf54ab4935a24e4ccafee5f03131e6d37bf46e6c
Amar2050 <amarboutayeb@gmail.com>
2021-01-06 14:12:10 +0100

    ROUND 1 - Clean du projet

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,30 +2,22 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home_page")
      */
-    public function index()
+    public function index(): Response
     {    
         return $this->render('home/index.html.twig', [
            
         ]);
     }
-
-    /**
-     * @Route("/articles", name="articles_list")
-     */
-    public function articlesList()
-    {    
-        return $this->render('home/articles.list.html.twig', [
-           
-        ]);
-    }
-
+    
 
 }
