11d1d4f68175b7c7c8dbca38045f984435f2009f
Amar2050 <amarboutayeb@gmail.com>
2021-01-12 17:04:13 +0100

    Article SHOW - Controller + View
    Via param converter

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -21,4 +21,19 @@ class ArticleController extends AbstractController
             'articles' => $articles
         ]);
     }
+
+    /**
+     * @Route("/articles/{slug}", name="article_show")
+     */
+    public function show($slug, ArticleRepository $articleRepository): Response
+    {
+
+        $article = $articleRepository->findOneBySlug($slug);
+
+        return $this->render('article/show.html.twig', [
+            'article' => $article
+        ]);
+    }
+
+
 }
