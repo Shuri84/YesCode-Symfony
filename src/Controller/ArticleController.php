c3f9f8c1fdb2ab18549bb08eb21b04a8a5454ebf
Amar2050 <amarboutayeb@gmail.com>
2021-01-12 09:57:09 +0100

    ROUND3 - TP done
    correction du tp , projet pret a continuer
    A suivre requete DQL ect ect ect

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/articles", name="articles_index")
+     */
+    public function index(ArticleRepository $articleRepository): Response
+    {
+
+        $articles = $articleRepository->findAll();
+
+        return $this->render('article/index.html.twig', [
+            'articles' => $articles
+        ]);
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 
+use App\Entity\Article;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -12,6 +13,15 @@ class AppFixtures extends Fixture
     {
 
         for ($i=0; $i < 10 ; $i++) { 
+           $article = new Article();
+           $article->setTitle("Article numéro: ". $i);
+           $article->setIntro("Ceci est une super intro");
+           $article->setContent("<p>Je suis du le 1er paragraphe</p>
+                                  <p>Je suis du le 2eme paragraphe</p>
+                                  <p>Je suis du le 3eme paragraphe</p>");
+            $article->setImage("https://media.cdnws.com/_i/85346/285/2264/87/blog.jpeg");
+            $article->setCreatedAt(new \DateTime());
+            $manager->persist($article);
 
         }
 
