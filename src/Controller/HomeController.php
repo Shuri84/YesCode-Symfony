154689383e274c567b2ba883b9693cf498fa28de
Amar2050 <amarboutayeb@gmail.com>
2021-01-12 12:22:23 +0100

    Fin du test Faker - Cleaning HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use Faker;
 use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,29 +15,10 @@ class HomeController extends AbstractController
      */
     public function index(ArticleRepository $repo): Response
     {    
-
         $articles = $repo->findLastArticles(3);
 
-        $faker = Faker\Factory::create('fr_FR');
-
-        // $title = $faker->sentence(2);
-
-        // $intro = $faker->paragraph(2);
-
-        // $contenu = ["pomme", "poire", "figue", "grenade", "test"];
-
-        $content = "<p>" . implode("</p><p>" , $faker->paragraphs(7) ) . "</p>";
-
-        $createdAt =  $faker->dateTimeBetween('- 3 months') ;
-
-        dump( $createdAt );
-
-        $image = "https://picsum.photos/400/300";
-        
         return $this->render('home/index.html.twig', [
-            "articles" => $articles,
-            "image" => $image,
-            "content" => $content
+            "articles" => $articles
         ]);
     }
     
